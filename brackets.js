115209ad0ec105072ef4d9c65f3394b636b4e607
Brian Green <briangreenery@gmail.com>
2014-02-10 15:49:15 -0800

    brackets

--- brackets.js
+++ brackets.js
@@ -1,18 +1,20 @@
 var Brackets = require('express')();
 
-var brackets = function (n) {
+var brackets = function (res, n) {
   var brackets = '';
   for (var i = 0; i < n; i++)
     brackets += '[]'[Math.floor(Math.random() * 2)];
-  return brackets;
+
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.send(brackets);
 };
 
 Brackets.get('/brackets', function (req, res) {
-  res.send(brackets(100));
+  brackets(res, 100);
 });
 
 Brackets.get('/brackets/:n', function (req, res) {
-  res.send(brackets(parseInt(req.params.n)));
+	brackets(res, parseInt(req.params.n));
 });
 
 Brackets.listen(process.env.PORT || 3000);
