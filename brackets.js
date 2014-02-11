7fd80f48ddbfcf0c3598d1c206d1c3767b1c679b
Brian Green <briangreenery@gmail.com>
2014-02-10 16:08:32 -0800

    brackets

--- brackets.js
+++ brackets.js
@@ -1,20 +1,33 @@
-var Brackets = require('express')();
+var app = require('express')();
+var Readable = require('stream').Readable;
 
-var brackets = function (res, n) {
-  var brackets = '';
-  for (var i = 0; i < n; i++)
-    brackets += '[]'[Math.floor(Math.random() * 2)];
+var brackets = function (n) {
 
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.send(brackets);
+  var stream = Readable();
+
+  stream._read = function() {
+    var chunk = '';
+
+    for (var i = 0; i < Math.min(n, 4096); i++)
+      chunk += '[ ]'[Math.floor(3 * Math.random())];
+
+    stream.push(chunk);
+    n -= chunk.length;
+
+    if (n == 0)
+      stream.push(null);
+  };
+
+  return stream;
 };
 
-Brackets.get('/brackets', function (req, res) {
-  brackets(res, 100);
+app.get('/brackets', function (req, res) {
+  brackets(100).pipe(res);
 });
 
-Brackets.get('/brackets/:n', function (req, res) {
-	brackets(res, parseInt(req.params.n));
+app.get('/brackets/:n', function (req, res) {
+  var count = Math.min(parseInt(req.params.n), 524288);
+  brackets(count).pipe(res);
 });
 
-Brackets.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
